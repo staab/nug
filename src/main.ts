1e3f64b4be8c65c9cc94cf2587f6ca2ffe9ad937
Jon Staab <shtaab@gmail.com>
2025-02-06 19:52:57 -0800

    Rip off svelte stores

--- src/main.ts
+++ src/main.ts
@@ -6,33 +6,37 @@ export const ensurePlural = <T>(x: T | T[]) => (x instanceof Array ? x : [x])
 
 export type Subscriber<T> = (value: T) => void
 
-export type Updater<T> = (value: T) => T
-
 export type Unsubscriber = () => void
+
+export type Updater<T> = (value: T) => T
 
 export type SubscribeOptions = {
   initial?: boolean
 }
 
-export class Store<T> {
-  private subs: Subscriber<T>[] = []
+export interface IReadable<T> {
+	get(): T
+	subscribe(run: Subscriber<T>, options: SubscribeOptions): Unsubscriber
+}
 
-  constructor(private value: T) {}
+export interface IWritable<T> extends IReadable<T> {
+	set(value: T): void
+	update(updater: Updater<T>): void
+}
+
+export type Stores = Array<IReadable<any>>
+
+export type StoresValues<T> = T extends IReadable<infer U>
+	? U
+	: { [K in keyof T]: T[K] extends IReadable<infer U> ? U : never };
+
+export class Readable<T> implements IReadable<T> {
+  protected subs: Subscriber<T>[] = []
+
+  constructor(protected value: T) {}
 
   get() {
     return this.value
-  }
-
-  set(value: T) {
-    this.value = value
-
-    for (const cb of this.subs) {
-      cb(this.value)
-    }
-  }
-
-  update(cb: Updater<T>) {
-    this.set(cb(this.value))
   }
 
   subscribe(cb: Subscriber<T>, {initial = true}: SubscribeOptions = {}) {
@@ -47,6 +51,71 @@ export class Store<T> {
     }
   }
 }
+
+export const readable = <T>(value: T | undefined) => new Readable(value)
+
+export class Writable<T> extends Readable<T> {
+  set(value: T) {
+    this.value = value
+
+    for (const cb of this.subs) {
+      cb(this.value)
+    }
+  }
+
+  update(cb: Updater<T>) {
+    this.set(cb(this.value))
+  }
+}
+
+export const writable = <T>(value: T | undefined) => new Writable(value)
+
+export class Derived<S extends Stores, T> implements IReadable<T> {
+  private subs: Subscriber<T>[] = []
+  private unsubscribers: Unsubscriber[] = []
+  private value: T
+
+  constructor(stores: S, cb: (values: StoresValues<S>) => T) {
+    const getStoresValues = () => stores.map(s => s.get()) as StoresValues<S>
+
+    this.value = cb(getStoresValues())
+
+    for (const store of stores) {
+      this.unsubscribers.push(
+        store.subscribe(() => {
+          this.value = cb(getStoresValues())
+        }, {
+          initial: false,
+        })
+      )
+    }
+  }
+
+  get() {
+    return this.value
+  }
+
+  subscribe(cb: Subscriber<T>, {initial = true}: SubscribeOptions = {}) {
+    this.subs.push(cb)
+
+    if (initial) {
+      cb(this.value)
+    }
+
+    return () => {
+      this.subs = this.subs.filter(s => s !== cb)
+    }
+  }
+
+  destroy() {
+    for (const unsub of this.unsubscribers) {
+      unsub()
+    }
+  }
+}
+
+export const derived = <S extends Stores, T>(stores: S, cb: (values: StoresValues<S>) => T) =>
+  new Derived(stores, cb)
 
 // Nug
 
@@ -129,7 +198,7 @@ export class NugUnsafe implements Nug {
 
 export type ComponentOptions<P extends ComponentProps> = {
   render: (props: P) => Nug[]
-  watch?: Store<any>[]
+  watch?: Readable<any>[]
 }
 
 export type ComponentProps = Record<string, any>
@@ -165,7 +234,7 @@ export class NugComponent<P extends ComponentProps> implements Nug {
     this.container = element
 
     for (const v of Object.values(this.props)) {
-      if (v instanceof Store) {
+      if (v.subscribe) {
         this.subs.push(v.subscribe(this.update, {initial: false}))
       }
     }
